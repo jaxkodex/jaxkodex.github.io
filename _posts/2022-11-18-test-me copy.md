0a2ff0f9532952ee342c6cd7b72c0f820c58f75b
Jorge Vilca <jorge.vilca@rimac.com.pe>
2022-11-18 22:19:38 +0000

    feat: configure styles

---
+++ _posts/2022-11-18-test-me copy.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "Hola Mundo!"
+subtitle: "Un blog gratis con Jekyll & Github Pages"
+summary: "This isa tes."
+image: /assets/images/markdown_v_html.PNG
+---
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./_layouts/**/*.{html,js}"],
+  content: ["./_layouts/**/*.{html,js}", "./_includes/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': '"Raleway", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+    },
     extend: {},
   },
   plugins: [],
